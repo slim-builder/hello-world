a9d6ac6afecdd57aaa37ae546e8db58f9bf81f08
Steven Lim <slim-builder@users.noreply.github.com>
2020-11-29 16:59:50 -0800

    Update test_hello.py

--- hello/test_hello.py
+++ hello/test_hello.py
@@ -2,14 +2,15 @@
 import unittest
 
 from hello import hello
-
+import time
 
 class TestHello(unittest.TestCase):
     """sample test"""
 
     def test_world(self):
         """sample test"""
-        self.assertEqual(hello('world'), 'hello worl')
+        time.sleep(30)
+        self.assertEqual(hello('worl'), 'hello worl')
 
     def test_world_unicode(self):
         """sample test with unicode"""
